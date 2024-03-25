c7bfd005f3eef207beb58ccf671235029e6285f3
kofnet002 <noeljoel61@gmail.com>
2024-03-25 19:32:38 +0000

    1. Add static files 2. Add vercel.json & build_files.sh for vercel deployment

--- core/settings.py
+++ core/settings.py
@@ -16,7 +16,7 @@ from datetime import timedelta
 import cloudinary
 import cloudinary_storage
 import dj_database_url
-
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -152,6 +152,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_URLS ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
