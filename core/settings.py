c1a019c9b6dc196c7f0e9f92e87fd5450852b158
kofnet002 <noeljoel61@gmail.com>
2024-05-10 15:14:39 +0000

    Prep to Heroku

--- core/settings.py
+++ core/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost', 'watchwave-watchwave.koyeb.app', 'watchwave-bbb50edab371.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1','localhost', 'watchwave-watchwave.koyeb.app', 'watchwave-bbb50edab371.herokuapp.com']
 CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'https://mywatchwave.vercel.app', 'https://watchwave-watchwave.koyeb.app', 'https://watchwave-bbb50edab371.herokuapp.com']
 
 
